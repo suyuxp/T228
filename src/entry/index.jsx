d43afb86980ff6506edf8278ac050f71e44cc57d
苏渝 <suyu@intermaker.cn>
2016-05-06 16:58:58 +0800

    index.jsx: url = /laws/api/v1

--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -13,14 +13,15 @@ import {
 }
 from "antd";
 
-const url = "http://127.0.0.1:8360";
+// const url = "http://127.0.0.1:8360";
+const url = "/laws/api/v1";
 
 class App extends React.Component {
 	render() {
 		return (
-			<div style={{display:"flex",flex:4}}> 
+			<div style={{display:"flex",flex:4}}>
 				<div style ={{flex:1,background:"#FFFFFF"}}>
-					<Categories url={url}/> 
+					<Categories url={url}/>
 				</div>
 				<div style ={{flex:3,paddingLeft:5,paddingRight:5,paddingTop:5,paddingBottom:5,}}>
 					<Texts url={url} />
